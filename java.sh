1c7087268da6e61fa2ca2f7a7dd3745808604990
albert v <cursodevops2018@gmail.com>
2018-05-28 12:16:12 +0200

    borrar bash.sh

--- java.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export PATH=/usr/java/jdk1.7.0_80/bin:$PATH
-JAVA_HOME="/usr/java/jdk1.7.0_80/"
-CLASSPATH=$JAVA_HOME/lib
-LD_LIBRARY_PATH=$JAVA_HOME/jre/lib/i386
-
-export JAVA_HOME CLASSPATH LD_LIBRARY_PATH
